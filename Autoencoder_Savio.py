26de4f8a212b410b9634d694cc3c7208918670e5
jagar2 <jagar@berkeley.edu>
2018-03-27 15:07:51 -0700

    updates for cannot read mat file

--- Autoencoder_Savio.py
+++ Autoencoder_Savio.py
@@ -3,28 +3,30 @@ from scipy import io
 import keras
 import numpy as np
 
-# loads the data file
-data = io.matlab.loadmat('./Data.mat')
+## loads the data file
+#data = io.matlab.loadmat('./Data.mat')
+#
+## extracts the voltage vector
+#voltage = data['Voltagedata_mixed']
+#
+## extracts the hyteresis loopsb
+#Piezoresponse = data['Loopdata_mixed']
+#Amplitude = data['OutA2_mixed']
+#Phase = data['OutPhi1_mixed']
+#Resonance = data['Outw2_mixed']
+#Quality = data['OutQ2_mixed']
+#
+## interpolates missing points
+#Piezoresponse = sf.interpolate_missing_points(Piezoresponse, 'linear')
+#Amplitude = sf.clean_and_interpolate(Amplitude, 'linear')
+#Phase = sf.clean_and_interpolate(Phase, 'linear')
+#Resonance = sf.clean_and_interpolate(Resonance, 'linear')
+#Quality = sf.clean_and_interpolate(Quality, 'linear')
+#
+#Resonance = sf.sg_filter_data(Resonance, fit_type='linear')
+#Resonance = sf.normalize_data(Resonance)
 
-# extracts the voltage vector
-voltage = data['Voltagedata_mixed']
-
-# extracts the hyteresis loopsb
-Piezoresponse = data['Loopdata_mixed']
-Amplitude = data['OutA2_mixed']
-Phase = data['OutPhi1_mixed']
-Resonance = data['Outw2_mixed']
-Quality = data['OutQ2_mixed']
-
-# interpolates missing points
-Piezoresponse = sf.interpolate_missing_points(Piezoresponse, 'linear')
-Amplitude = sf.clean_and_interpolate(Amplitude, 'linear')
-Phase = sf.clean_and_interpolate(Phase, 'linear')
-Resonance = sf.clean_and_interpolate(Resonance, 'linear')
-Quality = sf.clean_and_interpolate(Quality, 'linear')
-
-Resonance = sf.sg_filter_data(Resonance, fit_type='linear')
-Resonance = sf.normalize_data(Resonance)
+Resonance = np.load('resonance.npy')
 
 model, run_id = sf.rnn_auto('lstm', size=64, num_encode_layers = 4, num_decode_layers = 4,
                                         embedding = 16, n_step = 96, lr = 3e-5, drop_frac=0.2,
